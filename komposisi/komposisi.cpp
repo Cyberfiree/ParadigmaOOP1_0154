61280a2b9fdc5f29a50cc6bc924fea8479ca7249
Cyberfire <buayagoreng99@gmail.com>
2024-05-21 19:21:59 +0800

    membuat dan menghapus objek

--- komposisi/komposisi.cpp
+++ komposisi/komposisi.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+#include "Jantung.h"
+#include "manusia.h"
+
 int main()
 {
+	manusia* varManusia = new manusia("Jono"); // Membuat objek manusia secara dinamis dengan nama "Jono"
+	delete varManusia; //menghapus obyek yg telah dibuat // Menghapus objek manusia
+	return 0;
 }
