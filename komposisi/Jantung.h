988d087c4bfef4c052a4a14d53820ed7376e408c
Cyberfire <buayagoreng99@gmail.com>
2024-05-21 19:15:55 +0800

    membuat konstruktor dan destruktor jantung

--- komposisi/Jantung.h
+++ komposisi/Jantung.h
@@ -3,5 +3,15 @@
 
 class jantung { //kelas jantung
 public:
+
+	//Konstruktor Jantung
+	jantung() {
+		cout << "Jantung dihidupkan\n";
+	}
+
+	//// Destruktor Jantung
+	~jantung() {
+		cout << "Jantung dimatikan\n";
+	}
 };
 #endif
