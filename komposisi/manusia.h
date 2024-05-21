483169cc4d4a85018f1d8b2ce080a07f91ec1fac
Cyberfire <buayagoreng99@gmail.com>
2024-05-21 19:20:22 +0800

    membuat konstruktor dan destruktor manusia

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -6,5 +6,16 @@ class manusia {
 public:
 	string name;
 	jantung varJantung; //berelasi dan memanggil kelas jantung
+
+	// Konstruktor manusia
+	manusia(string pName)
+		: name(pName) {
+		cout << name << " hidup\n";
+	}
+
+	//destruktor manusia
+	~manusia() {
+		cout << name << " mati\n";
+	}
 };
 #endif
