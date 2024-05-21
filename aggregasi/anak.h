b78050b69d6535ce4991c692236db1fad4cd753a
Cyberfire <buayagoreng99@gmail.com>
2024-05-21 19:36:54 +0800

    membuat konstruktor dan destruktor

--- aggregasi/anak.h
+++ aggregasi/anak.h
@@ -3,5 +3,12 @@
 
 class anak {
 public:
+	string nama; // Attribut untuk menyimpan nama anak
+	anak(string pNama) : nama(pNama) {   // Konstruktor anak dengan satu parameter untuk menginisialisasi nama
+		cout << "Anak \"" << nama << "\" ada \n";
+	}
+	~anak() {   // Destruktor anak untuk memberikan pesan ketika anak dihancurkan
+		cout << "Anak \"" << nama << "\" tidak ada \n";
+	}
 };
 #endif
