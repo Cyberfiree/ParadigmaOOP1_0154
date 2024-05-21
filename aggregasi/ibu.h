4f5d6619fcd6177fe89127af7e5774f6c7351813
Cyberfire <buayagoreng99@gmail.com>
2024-05-21 19:43:40 +0800

    membuat kontruktor dan destruktor kelas ibu

--- aggregasi/ibu.h
+++ aggregasi/ibu.h
@@ -3,5 +3,18 @@
 
 class ibu {
 public:
+	string nama;
+	vector<anak*> daftar_anak; //memanggil kelas anak menggunakan vector //vector = list/array
+
+
+	// Konstruktor ibu dengan satu parameter untuk menginisialisasi nama
+	ibu(string pNama) : nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+
+	// Destruktor ibu untuk memberikan pesan ketika ibu dihapus
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	};
 };
 #endif
